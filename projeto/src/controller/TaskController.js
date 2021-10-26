404e154eb9b3e0db7d25d928a9f446e1c10712a8
wandlira <wanderson.lira1993@gmail.com>
2021-10-26 15:15:46 -0300

    readAll/readById/delete

--- projeto/src/controller/TaskController.js
+++ projeto/src/controller/TaskController.js
@@ -28,6 +28,41 @@ class TaskController{
 
     }
 
+
+    async readAll(req, res){
+
+    
+        await TaskModel.find({macadress: {'$in': req.body.macadress}})
+        .sort('when')
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+
+    }
+    
+    async readById(req, res){
+
+    
+        await TaskModel.findById(req.params.id)
+        .then(response => {
+            if(response)
+                return res.status(200).json(response)
+            else
+                return res.status(404).json({error : 'Tarefa não encontrada'})
+            })
+        .catch(error => {return res.status(500).json(error)});
+
+    }
+
+    async delete(req, res){
+
+    
+        await TaskModel.deleteOne({'_id' :req.params.id})
+        .then(response => {return res.status(200).json(response)})
+        .catch(error => {return res.status(500).json(error)});
+
+    }
+
+
 }
 
 
